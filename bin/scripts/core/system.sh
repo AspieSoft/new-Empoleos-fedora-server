30341e1763f34bc62a93c4817ab5ad4ede589925
AspieSoft <aspiesoftweb@gmail.com>
2023-09-18 14:03:19 -0400

    updated

--- bin/scripts/core/system.sh
+++ bin/scripts/core/system.sh
@@ -6,17 +6,18 @@ PackageManager="$3"
 
 
 # set bash profile $PS1
-if ! [ -f "etc/profile.d/bash_ps.sh" ]; then
-  echo 'if [ "$PS1" ]; then' | sudo tee -a "etc/profile.d/bash_ps.sh" &>/dev/null
-  echo '  PS1="\[\e[m\][\[\e[1;32m\]\u@\h\[\e[m\]:\[\e[1;34m\]\w\[\e[m\]]\[\e[0;31m\](\$?)\[\e[1;0m\]\\$ \[\e[m\]"' | sudo tee -a "etc/profile.d/bash_ps.sh" &>/dev/null
-  echo 'fi' | sudo tee -a "etc/profile.d/bash_ps.sh" &>/dev/null
+if ! [ -f "/etc/profile.d/bash_ps.sh" ]; then
+  echo 'if [ "$PS1" ]; then' >> /etc/profile.d/bash_ps.sh &>/dev/null
+  echo '  PS1="\[\e[m\][\[\e[1;32m\]\u@\h\[\e[m\]:\[\e[1;34m\]\w\[\e[m\]]\[\e[0;31m\](\$?)\[\e[1;0m\]\\$ \[\e[m\]"' >> /etc/profile.d/bash_ps.sh &>/dev/null
+  echo 'fi' >> /etc/profile.d/bash_ps.sh &>/dev/null
+  echo '' >> /etc/profile.d/bash_ps.sh &>/dev/null
+  echo 'if [ "$EUID" = "0" -a "$(stat -c %d:%i /)" != "$(stat -c %d:%i /proc/1/root/. 2>/dev/null)" ]; then' >> /etc/profile.d/bash_ps.sh &>/dev/null
+  echo '  PS1="\[\e[1;35m\](chroot)\[\e[m\]${PS1}"' >> /etc/profile.d/bash_ps.sh &>/dev/null
+  echo 'fi' >> /etc/profile.d/bash_ps.sh &>/dev/null
 fi
 
+# set hostname
+echo 'empoleos' > /etc/hostname
+hostname empoleos
 
-if [ "$(grep "^hostname=" /etc/conf.d/hostname)" != "" ]; then
-  sed -r -i 's/^hostname=".*"$/hostname="empoleos"/m' /etc/conf.d/hostname
-else
-  echo 'hostname="empoleos"' >> /etc/conf.d/hostname
-fi
-
-sed -r -i 's/^(127\.0\.0\.1|::1)(\s*)localhost$/\1\2empoleos localhost/m' /etc/hosts
+sed -r -i 's/^(127\.0\.0\.1|::1)(\s*)localhost(.*)$/\1\2empoleos localhost\3/m' /etc/hosts
