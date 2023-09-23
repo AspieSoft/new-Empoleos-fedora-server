8759d4739c598cf4353d1729a45c811fd26153e3
AspieSoft <aspiesoftweb@gmail.com>
2023-09-22 21:29:51 -0400

    updated

--- bin/scripts/dist/xfce/theme/apps.dnf.sh
+++ bin/scripts/dist/xfce/theme/apps.dnf.sh
@@ -12,7 +12,7 @@ sudo dnf -y install xfce4-diskperf-plugin
 # other
 sudo dnf -y install xfce4-whiskermenu-plugin
 
-# sudo dnf -y install xfce4-panel xfce4-panel-profiles
+sudo dnf -y install xfce4-panel xfce4-panel-profiles
 sudo dnf -y install xfce4-docklike-plugin
 
 
@@ -23,3 +23,14 @@ sudo dnf -y install xfce4-dev-tools
 sudo dnf -y install libxfce4util-devel
 sudo dnf -y install xfce4-sensors-plugin-devel
 sudo dnf -y install xfce4-appfinder
+
+sudo dnf -y install xfce4-verve-plugin
+sudo dnf -y install xfce4-genmon-plugin
+sudo dnf -y install xfce4-datetime-plugin
+sudo dnf -y install libxfce4ui
+
+sudo dnf -y install jgmenu-xfce4
+
+sudo dnf -y install gtk-xfce-engine
+
+sudo dnf -y install f38-backgrounds-xfce
